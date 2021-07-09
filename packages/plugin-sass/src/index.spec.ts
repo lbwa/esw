1a8051b19bf74312b8a1b4ae038085b69fadeed0
Liu Bowen <mr_lbw@outlook.com>
2021-07-09 21:59:02 +0800

    feat(sass): support sass implementations

--- packages/plugin-sass/src/index.spec.ts
+++ packages/plugin-sass/src/index.spec.ts
@@ -7,14 +7,14 @@ describe('sass plugin', () => {
   it('should work without bundle', async () => {
     const result = await build({
       entryPoints: ['./fixtures/index.sass'],
-      outdir: 'dist/without-bundle',
-      plugins: [sassPlugin],
+      outdir: 'lib/without-bundle',
+      plugins: [sassPlugin()],
       absWorkingDir: __dirname
     })
     expect(result.errors).toHaveLength(0)
     expect(result.warnings).toHaveLength(0)
     const output = fs.readFileSync(
-      path.resolve(__dirname, './dist/without-bundle/index.css'),
+      path.resolve(__dirname, './lib/without-bundle/index.css'),
       { encoding: 'utf8' }
     )
     expect(output).toMatchSnapshot('work without bundle')
--- packages/plugin-sass/src/index.ts
+++ packages/plugin-sass/src/index.ts
@@ -1,28 +1,54 @@
 import path from 'path'
-import sass from 'sass'
 import esbuild from 'esbuild'
+
+export type SassPluginOptions = {
+  implementation: 'sass' | 'node-sass'
+  baseDir: string
+}
 
 const SASS_NAMESPACE = 'style-sass'
 
-const plugin: esbuild.Plugin = {
-  name: 'plugin-sass',
-  setup(build) {
-    // define a resolver
-    build.onResolve({ filter: /\.s[ac]ss$/i }, args => ({
-      path: path.resolve(args.resolveDir, args.path),
-      namespace: SASS_NAMESPACE
-    }))
-
-    // define a loader
-    build.onLoad({ filter: /.*/, namespace: SASS_NAMESPACE }, args => {
-      const compiled = sass.renderSync({ file: args.path })
-      return {
-        contents: compiled.css.toString(),
-        loader: 'css'
-      }
-    })
+function loadSass(impl: SassPluginOptions['implementation'], baseDir: string) {
+  try {
+    const url = require.resolve(impl, { paths: [baseDir] })
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    return require(url) as typeof import('sass')
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error(
+      `Couldn't find module ${impl}, please ensure it has been installed. e.g. yarn add ${impl} -D`
+    )
+    process.exit(1)
   }
 }
 
-export default plugin
-module.exports = plugin
+function createPlugin(
+  {
+    implementation = 'sass',
+    baseDir = process.cwd()
+  }: SassPluginOptions = {} as SassPluginOptions
+): esbuild.Plugin {
+  const sass = loadSass(implementation, baseDir)
+  return {
+    name: 'plugin-sass',
+    setup(build) {
+      // define a resolver
+      build.onResolve({ filter: /\.s[ac]ss$/i }, args => ({
+        path: path.resolve(args.resolveDir, args.path),
+        namespace: SASS_NAMESPACE
+      }))
+
+      // define a loader
+      build.onLoad({ filter: /.*/, namespace: SASS_NAMESPACE }, args => {
+        const compiled = sass.renderSync({ file: args.path })
+        return {
+          contents: compiled.css.toString(),
+          loader: 'css'
+        }
+      })
+    }
+  }
+}
+
+export default createPlugin
+module.exports = createPlugin
