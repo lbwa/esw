5b5681d596f7c7cfab7e17d8329ec7131b680bcd
Liu Bowen <mr_lbw@outlook.com>
2021-07-19 23:21:40 +0800

    test: modify cases

--- packages/esw/test/build.spec.ts
+++ packages/esw/test/build.spec.ts
@@ -1,6 +1,7 @@
 import path from 'path'
 import fs from 'fs'
 import { exec, ExecException } from 'child_process'
+import { BuildOptions } from 'esbuild'
 import { build } from '../src'
 
 function getTestName() {
@@ -11,18 +12,50 @@ function resolveFixture(name: string) {
   return path.resolve(__dirname, `./fixture/${name}`)
 }
 
-const cacheDir = 'dist/build'
-const outDir = resolveFixture(`typescript/${cacheDir}`)
+function clearCacheDir(dir: string) {
+  return fs.promises.rm(dir, { force: true, recursive: true })
+}
+
+async function runFixtureCase(
+  fixtureName: string,
+  cacheDir: string,
+  options: BuildOptions
+) {
+  await clearCacheDir(resolveFixture(`${fixtureName}/${cacheDir}`))
+  const result = await build({
+    absWorkingDir: resolveFixture(fixtureName),
+    logLevel: 'debug',
+    format: 'esm',
+    entryPoints: ['index.ts'],
+    outdir: cacheDir,
+    bundle: false,
+    ...options
+  })
+  expect(result.errors).toHaveLength(0)
+  expect(result.warnings).toHaveLength(0)
+  expect(result).toMatchSnapshot(getTestName())
+
+  expect(
+    fs.existsSync(resolveFixture(`${fixtureName}/${cacheDir}/index.js`))
+  ).toBeTruthy()
+
+  return fs.promises.readFile(
+    resolveFixture(`${fixtureName}/${cacheDir}/index.js`),
+    { encoding: 'utf8' }
+  )
+}
 
 beforeAll(async () => {
-  await fs.promises.rm(outDir, { force: true, recursive: true })
   await new Promise<ExecException | null>(r => {
     exec('yarn', { cwd: resolveFixture('typescript') }, r)
   })
 })
 
 describe('node api - build', () => {
-  it('should work with package.json', async () => {
+  it('should work with package.json and dependencies', async () => {
+    const cacheDir = 'dist/build'
+    const outDir = resolveFixture(`typescript/${cacheDir}`)
+    await clearCacheDir(outDir)
     const result = await build({
       absWorkingDir: resolveFixture('typescript'),
       logLevel: 'debug',
@@ -54,53 +87,24 @@ describe('node api - build', () => {
     expect(cjsOutput).toContain(`require("rxjs/operators")`)
   })
 
-  it('should output cjs syntax', async () => {
-    const result = await build({
-      absWorkingDir: resolveFixture('typescript'),
-      logLevel: 'debug',
-      format: 'cjs',
-      entryPoints: ['src/cjs.ts'],
-      outdir: cacheDir
+  it('should work with main field and cjs syntax', async () => {
+    const output = await runFixtureCase('only-main-field', 'dist/cjs', {
+      format: 'cjs'
     })
-    expect(result.errors).toHaveLength(0)
-    expect(result.warnings).toHaveLength(0)
-    expect(result).toMatchSnapshot(getTestName())
-
-    expect(
-      fs.existsSync(resolveFixture(`typescript/${cacheDir}/cjs.js`))
-    ).toBeTruthy()
-
-    const output = await fs.promises.readFile(
-      resolveFixture(`typescript/${cacheDir}/cjs.js`),
-      { encoding: 'utf8' }
-    )
-    expect(output).toContain(`require("react")`)
-    expect(output).toContain(`require("rxjs")`)
-    expect(output).toContain(`require("rxjs/operators")`)
+    expect(output).toContain(`require("./src/fib")`)
   })
 
-  it('should output iife syntax', async () => {
-    const result = await build({
-      absWorkingDir: resolveFixture('typescript'),
-      logLevel: 'debug',
-      format: 'iife',
-      entryPoints: ['src/iife.ts'],
-      outdir: cacheDir
+  it('should work with main field and esm syntax', async () => {
+    const output = await runFixtureCase('only-main-field', 'dist/esm', {
+      format: 'esm'
     })
-    expect(result.errors).toHaveLength(0)
-    expect(result.warnings).toHaveLength(0)
-    expect(result).toMatchSnapshot(getTestName())
+    expect(output).toContain(`from "./src/fib"`)
+  })
 
-    expect(
-      fs.existsSync(resolveFixture(`typescript/${cacheDir}/iife.js`))
-    ).toBeTruthy()
-
-    const output = await fs.promises.readFile(
-      resolveFixture(`typescript/${cacheDir}/iife.js`),
-      { encoding: 'utf8' }
-    )
-    expect(output).toContain(`require("react")`)
-    expect(output).toContain(`require("rxjs")`)
-    expect(output).toContain(`require("rxjs/operators")`)
+  it('should work with module field and esm syntax', async () => {
+    const output = await runFixtureCase('only-module-field', 'dist', {
+      format: 'esm'
+    })
+    expect(output).toContain(`from "./src/fib"`)
   })
 })
