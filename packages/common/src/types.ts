a88b04b6752847ba90143f54a2188ff76b90f03c
Liu Bowen <mr_lbw@outlook.com>
2021-08-12 00:18:52 +0800

    build: only support cjs format output syntax
    Signed-off-by: Liu Bowen <mr_lbw@outlook.com>

--- packages/common/src/types.ts
+++ packages/common/src/types.ts
@@ -1,5 +1,5 @@
 import { Plugin as EsBuildPlugin } from 'esbuild'
 
 export interface EswPlugin<Options extends Record<string, unknown>> {
-  (options: Options): EsBuildPlugin
+  (options?: Options): EsBuildPlugin
 }
