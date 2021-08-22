8d7955e6f71606297b3c383b28cc2ce71cdbdf63
Liu Bowen <mr_lbw@outlook.com>
2021-08-23 00:18:46 +0800

    refactor(esw): make code more readable
    Signed-off-by: Liu Bowen <mr_lbw@outlook.com>

--- packages/esw/src/shared/observable.ts
+++
@@ -1,6 +0,0 @@
-import { Observable, of, map } from 'rxjs'
-
-export function lazyRequireObs<M = unknown>(filepath: string): Observable<M> {
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-  return of(filepath).pipe(map(p => require(p)))
-}
