8eae572d7622f74499438be6c513066ed4f5e322
Liu Bowen <mr_lbw@outlook.com>
2021-09-12 23:21:31 +0800

    feat(common): support esbuild note print

--- packages/common/tests/esbuild-msg.spec.ts
+++ packages/common/tests/esbuild-msg.spec.ts
@@ -70,4 +70,67 @@ describe('serialize build error', () => {
     spy.mockRestore()
     wrongSpy.mockRestore()
   })
+
+  it('should print note', async () => {
+    const spy = jest
+      .spyOn(process.stderr, 'write')
+      .mockImplementation(() => true)
+    const wrongSpy = jest.spyOn(process.stdout, 'write')
+
+    printBuildError({
+      errors: [
+        {
+          detail: undefined,
+          location: {
+            column: 58,
+            file: 'index.tsx',
+            length: 1,
+            line: 1,
+            lineText:
+              'let button = <Button content="some so-called \\"button text\\"" />',
+            namespace: '',
+            suggestion: ''
+          },
+          notes: [
+            {
+              location: {
+                column: 45,
+                file: 'index.tsx',
+                length: 2,
+                line: 1,
+                lineText:
+                  'let button = <Button content="some so-called \\"button text\\"" />',
+                namespace: '',
+                suggestion: '&quot;'
+              },
+              text: 'Quoted JSX attributes use XML-style escapes instead of JavaScript-style escapes'
+            },
+            {
+              location: {
+                column: 29,
+                file: 'index.tsx',
+                length: 32,
+                line: 1,
+                lineText:
+                  'let button = <Button content="some so-called \\"button text\\"" />',
+                namespace: '',
+                suggestion: '{"some so-called \\"button text\\""}'
+              },
+              text: 'Consider using a JavaScript string inside {...} instead of a quoted JSX attribute'
+            }
+          ],
+          pluginName: '',
+          text: 'Unexpected backslash in JSX element'
+        }
+      ],
+      warnings: [],
+      name: 'Error',
+      message: ''
+    })
+
+    expect(wrongSpy).not.toHaveBeenCalled()
+    expect(spy).toBeCalledTimes(1)
+    expect(spy.mock.calls[0]?.join('\n')).toMatchSnapshot(`stderr`)
+    spy.mockRestore()
+  })
 })
