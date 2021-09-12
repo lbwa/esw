cc1be7358ba11363a3f3418c2d0fafe58cd004a6
Liu Bowen <mr_lbw@outlook.com>
2021-09-12 19:14:29 +0800

    feat(common): printToTerminal

--- packages/common/src/const.ts
+++ packages/common/src/const.ts
@@ -1,1 +1,9 @@
 export const isEnvTest = process.env['NODE_ENV'] === 'test'
+
+/**
+ * @see https://nodejs.org/api/process.html#process_exit_codes
+ */
+export enum ExitCode {
+  OK,
+  ERROR
+}
--- packages/common/src/stdout.ts
+++ packages/common/src/stdout.ts
@@ -1,5 +1,7 @@
 /* eslint-disable no-console */
 import chalk from './chalk'
+import { ExitCode } from './const'
+import { isDef } from '.'
 
 export function wait(...message: string[]): void {
   console.log(chalk.bgCyan.black(' WAIT '), ...message)
@@ -11,4 +13,19 @@ export function error(...message: string[]) {
 
 export function warn(...message: string[]) {
   console.warn(chalk.bgYellow.black(' WARN '), ...message)
+}
+
+export function printToTerminal(
+  message: string,
+  code: number = ExitCode.OK,
+  exit = false
+) {
+  if (code === ExitCode.OK) {
+    process.stdout.write(message)
+  } else if (isDef(code)) {
+    process.stderr.write(message)
+  }
+  if (exit) {
+    process.exit(code)
+  }
 }
