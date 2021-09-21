13d0baf95f702339664be4c856c0487165bb5ed4
Liu Bowen <mr_lbw@outlook.com>
2021-09-21 18:05:15 +0800

    refactor(esw): use Build class to encapsulate build logic

--- packages/esw/src/build.ts
+++ packages/esw/src/build.ts
@@ -1,6 +1,6 @@
 import fs from 'fs'
 import { build, BuildOptions } from 'esbuild'
-import { map, tap, mergeMap, toArray, firstValueFrom, pipe } from 'rxjs'
+import { map, tap, mergeMap, toArray, firstValueFrom, pipe, of } from 'rxjs'
 import { isDef } from '@eswjs/common'
 import { inferBuildOption } from './observable/build-options'
 
@@ -27,51 +27,65 @@ function checkBuildOptions<Options extends BuildOptions>() {
 
 type EsBuildInternalOutputPath = string
 type DestinationPathFromPackageJson = string
-export const outputPathMapping = new Map<
-  EsBuildInternalOutputPath,
-  DestinationPathFromPackageJson[]
->()
+
+export class Build {
+  pathsMap = new Map<
+    EsBuildInternalOutputPath,
+    DestinationPathFromPackageJson[]
+  >()
+  options$ = of({} as BuildOptions)
+
+  constructor(
+    options: BuildOptions = {},
+    cwd: string = options.absWorkingDir || process.cwd()
+  ) {
+    this.options$ = inferBuildOption(options, cwd, this.pathsMap)
+  }
+
+  run() {
+    const run$ = this.options$.pipe(
+      checkBuildOptions(),
+      toArray(),
+      mergeMap(async options => {
+        if (options.length < 1) {
+          throw new Error(
+            `Invalid operation. Maybe you forgot to define the main or module field in the package.json file.`
+          )
+        }
+
+        const pending = [] as string[]
+        await Promise.all(
+          options.map(async ({ outdir }) => {
+            if (!outdir || !/^(?:\.\/)?[a-zA-Z0-9]+/i.test(outdir)) return
+
+            const stat = await fs.promises.lstat(outdir).catch(() => null)
+            if (
+              isDef(stat) &&
+              stat.isDirectory() &&
+              !pending.includes(outdir)
+            ) {
+              pending.push(outdir)
+            }
+          })
+        )
+        await Promise.all(
+          pending.map(outdir =>
+            fs.promises.rm(outdir, { recursive: true, force: true })
+          )
+        )
+
+        return options
+      }),
+      map(options => options.map(options => build(options))),
+      mergeMap(handles => Promise.allSettled(handles))
+    )
+    return firstValueFrom(run$)
+  }
+}
 
 export default function runBuild(
   options: BuildOptions = {},
   cwd: string = options.absWorkingDir || process.cwd()
 ) {
-  const invokeEsBuildBuild$ = inferBuildOption(
-    options,
-    cwd,
-    outputPathMapping
-  ).pipe(
-    checkBuildOptions(),
-    toArray(),
-    mergeMap(async options => {
-      if (options.length < 1) {
-        throw new Error(
-          `Invalid operation. Maybe you forgot to define the main or module field in the package.json file.`
-        )
-      }
-
-      const pending = [] as string[]
-      await Promise.all(
-        options.map(async ({ outdir }) => {
-          if (!outdir || !/^(?:\.\/)?[a-zA-Z0-9]+/i.test(outdir)) return
-
-          const stat = await fs.promises.lstat(outdir).catch(() => null)
-          if (isDef(stat) && stat.isDirectory() && !pending.includes(outdir)) {
-            pending.push(outdir)
-          }
-        })
-      )
-      await Promise.all(
-        pending.map(outdir =>
-          fs.promises.rm(outdir, { recursive: true, force: true })
-        )
-      )
-
-      return options
-    }),
-    map(options => options.map(options => build(options))),
-    mergeMap(handles => Promise.allSettled(handles))
-  )
-
-  return firstValueFrom(invokeEsBuildBuild$)
+  return new Build(options, cwd).run()
 }
