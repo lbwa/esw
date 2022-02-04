e14e6b1395376da237db62c0d10a5bb2f919c707
Liu Bowen <mr_lbw@outlook.com>
2022-02-04 13:53:22 +0800

    test: rename cli.spec -> build.spec

--- packages/esw/test/e2e/build.spec.ts
+++ packages/esw/test/e2e/build.spec.ts
@@ -1,278 +1,143 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-import fs from 'fs/promises'
 import path from 'path'
-import * as esbuild from 'esbuild'
-import identity from 'lodash/identity'
-import pick from 'lodash/pick'
-import { PackageJson } from 'type-fest'
-import {
-  resolveFixture,
-  getTestName,
-  formatBuildResult,
-  hasErrors,
-  hasWarnings
-} from './shared'
-import { build } from '@root/index'
+import { format } from 'util'
+import fs from 'fs'
+import { createCliDriver } from './driver/cli'
+import { createSandbox, Sandbox } from './sandbox/sandbox'
 
-async function readOutputs(cwd: string) {
-  const { main: cjsPath, module: esmPath } = require(path.resolve(
-    cwd,
-    'package.json'
-  )) as PackageJson
-  return Promise.all(
-    ([cjsPath, esmPath].filter(Boolean) as string[]).map(p =>
-      fs.readFile(path.resolve(cwd, p), { encoding: 'utf8' })
-    )
-  )
-}
-
-async function runFixtureCase(
-  fixtureName: string,
-  options: esbuild.BuildOptions,
-  singleEntry = 'index.ts'
-) {
-  const cwd = resolveFixture(fixtureName)
-  const results = await build({
-    absWorkingDir: cwd,
-    logLevel: 'debug',
-    format: 'esm',
-    entryPoints: [singleEntry],
-    bundle: false,
-    ...options
-  })
-  const buildResults = formatBuildResult(results, identity)
-  expect(hasErrors(buildResults)).toBeFalsy()
-  expect(hasWarnings(buildResults)).toBeFalsy()
-  expect(
-    buildResults.map(result => pick(result, 'warnings', 'errors'))
-  ).toMatchSnapshot(getTestName())
-
-  return readOutputs(cwd)
-}
-
-describe('build api', () => {
-  it('should mark all peerDependencies and dependencies as external', async () => {
-    const cwd = resolveFixture('with-deps')
-    const results = await build({
-      absWorkingDir: cwd,
-      logLevel: 'debug'
+describe('cli stdout', () => {
+  let sandbox: Sandbox
+  beforeAll(async () => {
+    sandbox = await createSandbox()
+    await sandbox.load(path.resolve(__dirname, 'fixtures/no-options'))
+    await sandbox.install({
+      esw: format('file:%s', process.env.E2E_LIB_PACK_PATH)
     })
-    const buildResults = formatBuildResult(results, identity)
-    expect(buildResults).toHaveLength(2)
-    expect(hasErrors(buildResults)).toBeFalsy()
-    expect(hasWarnings(buildResults)).toBeFalsy()
-    expect(
-      buildResults.map(result => pick(result, 'warnings', 'errors'))
-    ).toMatchSnapshot(getTestName())
-
-    const { main: cjsPath, module: esmPath } = require(path.resolve(
-      cwd,
-      'package.json'
-    )) as PackageJson
-    const [cjsOutput, esmOutput] = await Promise.all([
-      fs.readFile(path.resolve(cwd, cjsPath as string), { encoding: 'utf8' }),
-      fs.readFile(path.resolve(cwd, esmPath as string), { encoding: 'utf8' })
-    ])
-
-    expect(esmOutput).toContain(`from "react"`)
-    expect(esmOutput).toContain(`from "rxjs"`)
-    expect(esmOutput).toContain(`from "rxjs/operators"`)
-
-    expect(cjsOutput).toContain(`require("react")`)
-    expect(cjsOutput).toContain(`require("rxjs")`)
-    expect(cjsOutput).toContain(`require("rxjs/operators")`)
   })
 
-  it('should work with main field and cjs syntax', async () => {
-    const [output] = await runFixtureCase('only-main-field', {
-      format: 'cjs'
-    })
-    expect(output).toContain(`require("./src/fib")`)
+  beforeEach(async () => {
+    return sandbox.reset()
   })
 
-  it('should work with main field and esm syntax', async () => {
-    const [output] = await runFixtureCase('only-main-field-esm', {
-      // should respect option.format
-      format: 'esm'
-    })
-    expect(output).toContain(`from "./src/fib"`)
+  afterAll(async () => {
+    return sandbox.terminate()
   })
 
-  it("shouldn't work with main field and cjs syntax without type: commonjs", async () => {
-    await expect(
-      runFixtureCase('only-main-field-esm', {
-        format: 'cjs'
-      })
-    ).rejects.toThrowErrorMatchingSnapshot(getTestName())
+  it.each([
+    ['print global message', ['--help']],
+    ['print global message by alias', ['-h']],
+    ['print global message with unknown command', ['unknownCommand']],
+    ['print version', ['--version']],
+    ['print version by alias', ['-v']],
+    ['print build usage', ['build', '--help']],
+    ['print build usage by alias', ['build', '-h']]
+  ])('should %s', async (name, args) => {
+    const driver = createCliDriver(sandbox.spawn('esw', args))
+    const stdout = await driver.waitForStdout()
+    expect(stdout).toMatchSnapshot(name)
+  })
+})
+
+describe('esw build', () => {
+  let sandbox: Sandbox
+  beforeAll(async () => {
+    sandbox = await createSandbox()
   })
 
-  it("shouldn't work with main field and esm syntax without type: module", async () => {
-    await expect(
-      runFixtureCase('only-main-field', {
-        format: 'esm'
-      })
-    ).rejects.toThrowErrorMatchingSnapshot(getTestName())
+  beforeEach(async () => {
+    return sandbox.reset()
   })
 
-  it('should work with module field and esm syntax', async () => {
-    const [output] = await runFixtureCase('only-module-field', {}, 'index.ts')
-    expect(output).toContain(`from "./src/fib"`)
-  })
-
-  it("shouldn't override the esm inference from module field", async () => {
-    const [output] = await runFixtureCase(
-      'only-module-field',
-      { format: 'cjs' },
-      'index.ts'
-    )
-    expect(output).toContain(`from "./src/fib"`)
-  })
-
-  it('should override the cjs inference from main field', async () => {
-    const [output] = await runFixtureCase(
-      'only-module-field',
-      { format: 'esm' },
-      'index.ts'
-    )
-    expect(output).toContain(`from "./src/fib"`)
-  })
-
-  it("shouldn't work without package.json file", async () => {
-    let err: Error | null = null
-    await build({
-      absWorkingDir: resolveFixture('required-package.json'),
-      logLevel: 'debug',
-      format: 'esm',
-      entryPoints: ['index.ts'],
-      outdir: 'dist',
-      bundle: false
-    }).catch((error: Error) => {
-      err = error
-    })
-    expect(err).not.toBeNull()
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    expect(err!.message).toContain(`"package.json" is required`)
+  afterAll(async () => {
+    return sandbox.terminate()
   })
 
   it('should work with no options', async () => {
-    await Promise.all(
-      ['index.esm.js', 'index.js'].map(file =>
-        fs.rm(path.resolve(__dirname, 'fixtures/no-options', file), {
-          force: true,
-          recursive: true
-        })
-      )
+    await sandbox.load(path.resolve(__dirname, 'fixtures/no-options'))
+    await sandbox.install({
+      esw: format('file:%s', process.env.E2E_LIB_PACK_PATH)
+    })
+    const driver = createCliDriver(sandbox.spawn('esw', ['build']))
+    const result = await driver.waitForStdout()
+    expect(result).toMatchSnapshot()
+
+    const esmOutPath = path.resolve(sandbox.cwd, 'index.esm.js'),
+      cjsOutPath = path.resolve(sandbox.cwd, 'dist/cjs/index.js')
+    const esmContent = await fs.promises.readFile(esmOutPath, 'utf8')
+    expect(esmContent).not.toContain('__esModule')
+    expect(esmContent).not.toContain('module.exports')
+    expect(esmContent).toMatch(/^export\s/m)
+    const cjsContent = await fs.promises.readFile(cjsOutPath, 'utf8')
+    expect(cjsContent).toContain('__esModule')
+    expect(cjsContent).toContain('module.exports')
+    expect(cjsContent).not.toMatch(/^export\s/)
+  })
+
+  it('should mark all dependencies/peerDependencies as external', async () => {
+    await sandbox.load(path.resolve(__dirname, 'fixtures/with-deps'))
+    await sandbox.install({
+      esw: format('file:%s', process.env.E2E_LIB_PACK_PATH)
+    })
+    const driver = createCliDriver(sandbox.spawn('esw', ['build']))
+    const result = await driver.waitForStdout()
+    expect(result).toMatchSnapshot()
+
+    const esmOutPath = path.resolve(sandbox.cwd, 'dist/index.esm.js'),
+      cjsOutPath = path.resolve(sandbox.cwd, 'dist/index.js')
+    const [esmContent, cjsContent] = await Promise.all(
+      [esmOutPath, cjsOutPath].map(p => fs.promises.readFile(p, 'utf8'))
     )
 
-    const fixtureName = 'no-options'
-    const cwd = resolveFixture(fixtureName)
-    const results = await build({
-      absWorkingDir: cwd,
-      logLevel: 'debug'
+    expect(esmContent).toContain(`from "react"`)
+    expect(esmContent).toContain(`from "rxjs"`)
+    expect(esmContent).toContain(`from "rxjs/operators"`)
+
+    expect(cjsContent).toContain(`require("react")`)
+    expect(cjsContent).toContain(`require("rxjs")`)
+    expect(cjsContent).toContain(`require("rxjs/operators")`)
+  })
+
+  it('should throw a error when esbuild bundling failed', async () => {
+    await sandbox.load(path.resolve(__dirname, 'fixtures/build-error'))
+    await sandbox.install({
+      esw: format('file:%s', process.env.E2E_LIB_PACK_PATH)
     })
-    const buildResults = formatBuildResult(results, identity)
-    expect(hasErrors(buildResults)).toBeFalsy()
-    expect(hasWarnings(buildResults)).toBeFalsy()
-    expect(
-      buildResults.map(result => pick(result, 'warnings', 'errors'))
-    ).toMatchSnapshot(getTestName())
 
-    const [cjsOutput, esmOutput] = await readOutputs(cwd)
+    const driver = createCliDriver(sandbox.spawn('esw', ['build']))
+    const error = await driver.waitForStderr()
 
-    expect(cjsOutput).toContain(`__esModule"`)
-    expect(esmOutput).not.toContain(`__esModule`)
+    expect(error).toMatch(/could not resolve/i)
   })
 
-  it('should work with no options and main field', async () => {
-    const cwd = resolveFixture('no-options-with-main')
-    const results = await build({
-      absWorkingDir: cwd,
-      logLevel: 'debug'
+  it('should clean dist first before next bundling start.', async () => {
+    await sandbox.load(path.resolve(__dirname, 'fixtures/clean-dist'))
+    await sandbox.install({
+      esw: format('file:%s', process.env.E2E_LIB_PACK_PATH)
     })
-    const buildResults = formatBuildResult(results, identity)
-    expect(buildResults).toHaveLength(1)
-    expect(hasErrors(buildResults)).toBeFalsy()
-    expect(hasWarnings(buildResults)).toBeFalsy()
-    expect(
-      buildResults.map(result => pick(result, 'warnings', 'errors'))
-    ).toMatchSnapshot(getTestName())
 
-    const [cjsOutput] = await readOutputs(cwd)
-    expect(cjsOutput).toContain(`__esModule"`)
-  })
+    const cacheFile = path.resolve(sandbox.cwd, 'dist/cache.ts')
+    await fs.promises.mkdir(path.dirname(cacheFile), { recursive: true })
+    await fs.promises.writeFile(
+      cacheFile,
+      'export const message = "should be deleted."'
+    )
+    expect(fs.existsSync(cacheFile)).toBeTruthy()
 
-  it('should work with no options and module field', async () => {
-    const cwd = resolveFixture('no-options-with-module')
-    const results = await build({
-      absWorkingDir: cwd,
-      logLevel: 'debug'
-    })
-    const buildResults = formatBuildResult(results, identity)
-    expect(buildResults).toHaveLength(1)
-    expect(hasErrors(buildResults)).toBeFalsy()
-    expect(hasWarnings(buildResults)).toBeFalsy()
-    expect(
-      buildResults.map(result => pick(result, 'warnings', 'errors'))
-    ).toMatchSnapshot(getTestName())
+    const driver = createCliDriver(sandbox.spawn('esw', ['build']))
 
-    const [esmOutput] = await readOutputs(cwd)
-    expect(esmOutput).not.toContain(`__esModule`)
-  })
+    await driver.waitForStdout()
 
-  it("shouldn't work with no valid entry field", async () => {
-    const fixtureName = 'no-valid-entry-field'
-    await expect(
-      build({
-        absWorkingDir: resolveFixture(fixtureName),
-        logLevel: 'debug'
-      })
-    ).rejects.toThrowErrorMatchingSnapshot(getTestName())
-  })
+    expect(fs.existsSync(cacheFile)).toBeFalsy()
+    const esmOutPath = path.resolve(sandbox.cwd, 'dist/index.esm.js'),
+      cjsOutPath = path.resolve(sandbox.cwd, 'dist/index.js')
+    const [esmContent, cjsContent] = await Promise.all(
+      [esmOutPath, cjsOutPath].map(p => fs.promises.readFile(p, 'utf8'))
+    )
 
-  it('should emit building only once when we have duplicated output path', async () => {
-    const result = await build({
-      absWorkingDir: resolveFixture('duplicated-entry-points'),
-      logLevel: 'debug'
-    })
-    expect(result).toHaveLength(1)
-  })
-
-  it('should emit a error when splitting: true and format !== esm', async () => {
-    await expect(
-      build({
-        absWorkingDir: resolveFixture('no-options'),
-        splitting: true,
-        format: 'cjs'
-      })
-    ).rejects.toThrowErrorMatchingSnapshot('splitting: true and format !== esm')
-  })
-
-  it("shouldn't emit a error when write is true", async () => {
-    await expect(
-      build({
-        absWorkingDir: resolveFixture('no-options'),
-        logLevel: 'debug',
-        write: true
-      })
-    ).resolves.toMatchSnapshot('works')
-  })
-
-  it("shouldn't emit rebuild when incremental is true", async () => {
-    await expect(
-      build({
-        absWorkingDir: resolveFixture('no-options'),
-        logLevel: 'debug',
-        incremental: true
-      })
-    ).rejects.toThrowError('only works with "watch" command')
-  })
-
-  it('should emit e error when entryPoints.lenth === 0', async () => {
-    await expect(
-      build({
-        absWorkingDir: resolveFixture('empty-entry'),
-        logLevel: 'debug'
-      })
-    ).rejects.toThrowError("couldn't infer the start point")
+    expect(esmContent).not.toContain('__esModule')
+    expect(esmContent).not.toContain('module.exports')
+    expect(esmContent).not.toContain('exports')
+    expect(esmContent).toMatch(/^export\s/m)
+    expect(cjsContent).toContain('__esModule')
+    expect(cjsContent).toContain('module.exports')
   })
 })
